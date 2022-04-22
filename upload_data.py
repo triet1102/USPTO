ce98628bd549651ee81c6ad6e40f89d74e7bc818
Triet TRAN <t.triet.1102@gmail.com>
2022-04-22 13:22:02 +0000

    upload data to datastore

---
+++ upload_data.py
@@ -0,0 +1,17 @@
+from azureml.core import Workspace
+from azureml.core import Dataset
+from azureml.data.datapath import DataPath
+import sys
+
+
+def main():
+    ws = Workspace.from_config()
+    datastore = ws.get_default_datastore()
+    Dataset.File.upload_directory(src_dir='data', 
+                                target=DataPath(datastore, "datasets/USPTO_data")
+                                )  
+
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
