305bef13726629158e5e1d0809769cdb73c264b0
Triet TRAN <t.triet.1102@gmail.com>
2022-05-05 10:17:24 +0000

    save_pretrained to reload on kaggle

--- predict_model.py
+++ predict_model.py
@@ -1,6 +1,6 @@
 from src.model import PhraseSimilarityModelImpl, PhraseSimilarityModel
 from src.sim_dataset import PhraseSimilarityTestset
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModel
 import sys
 import pandas as pd
 from torch.utils.data import DataLoader
@@ -8,37 +8,46 @@ from pytorch_lightning import Trainer
 import torch.nn as nn
 from torchmetrics import MeanSquaredError
 import yaml
+import os
 
 
 def main(model_ckpt, test_data_path, yaml_path):
     """Predict and fill submission.csv
     """
+    os.chdir("/home/azureuser/cloudfiles/code/Users/triet.tran/USPTO")
     with open(yaml_path, 'r') as f:
         hparams = yaml.safe_load(f)
     criterion = nn.HuberLoss(reduction='mean', delta=1.0)
     metric = MeanSquaredError()
     model = PhraseSimilarityModelImpl("bert-base-uncased")
-    print("OK1")
-    # driver = PhraseSimilarityModel(model, lr, criterion, metric)
-    print("OK2")
 
-    driver = PhraseSimilarityModel.load_from_checkpoint(model_ckpt,
-                                         model=model,
-                                         lr=hparams["learning_rate"],
-                                         criterion=criterion,
-                                         metric=metric)
-    print("OK3")
+    driver = PhraseSimilarityModel(model=model,
+                                   lr=hparams["learning_rate"],
+                                   criterion=criterion,
+                                   metric=metric)
 
-    # driver = PhraseSimilarityModel.load_from_checkpoint(
-    #     model_ckpt)
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+    test_data = pd.read_csv(test_data_path)
+    test_dataset = PhraseSimilarityTestset(test_data, tokenizer, max_length=64)
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=32, shuffle=False)
 
-    # print("ok")
-    # tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+    trainer = Trainer()
+    predictions = trainer.predict(
+        driver, dataloaders=test_dataloader, ckpt_path=model_ckpt)
+    preds = []
+    for batch in predictions:
+        preds += batch.squeeze(1).tolist()
 
-    # test_data = pd.read_csv(test_data_path)
-    # test_dataset = PhraseSimilarityTestset(test_data, tokenizer, max_length=64)
-    # test_dataloader = DataLoader(
-    #     test_dataset, batch_size=32, shuffle=False)
+    submission_csv = pd.read_csv("data/sample_submission.csv")
+    submission_csv["score"] = preds
+    print(submission_csv.head())
+
+    # bert_base_model = AutoModel.from_pretrained("bert-base-uncased")
+    # tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+    # bert_base_model.save_pretrained("saved_models/bert_base_uncased")
+    # tokenizer.save_pretrained("saved_models/bert_base_uncased")
 
 
 if __name__ == "__main__":
