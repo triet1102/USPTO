6561da1cd19df1445db2f12a77af6d0e10a90838
Triet TRAN <t.triet.1102@gmail.com>
2022-05-06 14:05:03 +0000

    extract code and titles from cpc-*.txt

--- src/util.py
+++ src/util.py
@@ -1,1 +1,34 @@
-# Implement code for data preprocessing
+import pandas as pd
+import os
+
+
+def preprocess_CPC(cpc_path):
+    """Extract titles coresspond to code
+
+    To download dataset
+    wget https://www.cooperativepatentclassification.org/sites/default/files/cpc/bulk/CPCTitleList202205.zip
+
+    :param cpc_path: Path of directory that contains cpc data
+    """
+    parsed = {x: [] for x in ["code", "title"]}
+    for letter in "ABCDEFGHY":
+        file = f"{cpc_path}/cpc-section-{letter}_20220501.txt"
+        with open(file) as f:
+            for line in f:
+                vals = line.strip().split('\t')
+                if len(vals[0]) == 3:
+                    parsed["code"].append(vals[0])
+                    parsed["title"].append(vals[2].lower())
+
+    df = pd.DataFrame.from_dict(parsed)
+    print(df.head())
+    df.to_csv(f"{cpc_path}/../CPC_context.csv")
+
+
+def main(cpc_path):
+    preprocess_CPC(cpc_path)
+
+
+if __name__ == "__main__":
+    CPC_PATH = "../data/CPCTitleList"
+    main(cpc_path=CPC_PATH)
